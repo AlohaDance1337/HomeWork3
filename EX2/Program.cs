42210d54f8894f55da4a4b3e2574cf02d1482d31
AlohaDance1337 <121186810+AlohaDance1337@users.noreply.github.com>
2023-02-10 20:37:53 +0300

    Add files via upload

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,4 +1,4 @@
-Console.WriteLine("Введите координаты точки А:");
+﻿Console.WriteLine("Введите координаты точки А:");
 String stringValue1 = Console.ReadLine();   
 String stringValue2 = Console.ReadLine();
 String stringValue3 = Console.ReadLine();
@@ -18,7 +18,7 @@ double Length(int x1, int y1, int z1 ,int x2, int y2, int z2)
 {
     double l = Math.Pow((Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2)*1), 0.5);
     Console.Write($"Полученное значение длинны {l}");
-    return;
+    return l;
 }
 
 Length(x1, y1 , z1, x2, y2, z2);
